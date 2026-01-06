830c393003c249f8b1917498c25b2cebf5d25696
up up up! <55922157+MJ-0515@users.noreply.github.com>
2026-01-06 15:13:48 +0800

    Update robust_evaluation.py

--- robust_evaluation.py
+++ robust_evaluation.py
@@ -33,11 +33,15 @@ def main():
     metrics = MetricsTop(train_mode = args['base']['train_mode']).getMetics(dataset_name)
 
     missing_rate_list = [0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+    result_avg1 = 0
+    result_arg2 = 0
+
     for cur_r in missing_rate_list:
         test_results_list = []
         if dataset_name == 'sims':
-            for _, cur_seed  in enumerate([1111, 1112, 1113]):
-                best_ckpt = os.path.join(f'ckpt/{dataset_name}/best_{key_eval}_{cur_seed}.pth')
+            for _, cur_seed  in enumerate([1113]):
+                best_ckpt = os.path.join(f'/root/LNLN-main/ckpt/{dataset_name}/best_{key_eval}_{cur_seed}.pth')
                 model.load_state_dict(torch.load(best_ckpt)['state_dict'])
                 args['base']['missing_rate_eval_test'] = cur_r # Set missing rate
 
@@ -49,23 +53,31 @@ def main():
                 test_results_list.append(test_results_cur_seed)
 
             if key_eval == 'Mult_acc_2':
-                Mult_acc_2_avg = (test_results_list[0]['Mult_acc_2'] + test_results_list[1]['Mult_acc_2'] + test_results_list[2]['Mult_acc_2']) / 3
-                F1_score_avg = (test_results_list[0]['F1_score'] + test_results_list[1]['F1_score'] + test_results_list[2]['F1_score']) / 3
+                Mult_acc_2_avg = test_results_list[0]['Mult_acc_2']
+                F1_score_avg = test_results_list[0]['F1_score'] 
+                result_avg1 += Mult_acc_2_avg
+                result_arg2 += F1_score_avg
                 print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_2_avg: {Mult_acc_2_avg}, F1_score_avg: {F1_score_avg}')
             elif key_eval == 'Mult_acc_3':
-                Mult_acc_3_avg = (test_results_list[0]['Mult_acc_3'] + test_results_list[1]['Mult_acc_3'] + test_results_list[2]['Mult_acc_3']) / 3
+                Mult_acc_3_avg = test_results_list[0]['Mult_acc_3'] 
+                result_avg1 += Mult_acc_3_avg
                 print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_3_avg: {Mult_acc_3_avg}')
             elif key_eval == 'Mult_acc_5':
-                Mult_acc_5_avg = (test_results_list[0]['Mult_acc_5'] + test_results_list[1]['Mult_acc_5'] + test_results_list[2]['Mult_acc_5']) / 3
+                Mult_acc_5_avg = test_results_list[0]['Mult_acc_5']
+                result_avg1 += Mult_acc_5_avg
                 print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_5_avg: {Mult_acc_5_avg}')
             elif key_eval == 'MAE':
-                MAE_avg = (test_results_list[0]['MAE'] + test_results_list[1]['MAE'] + test_results_list[2]['MAE']) / 3
-                Corr_avg = (test_results_list[0]['Corr'] + test_results_list[1]['Corr'] + test_results_list[2]['Corr']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, MAE_avg: {MAE_avg}, Corr_avg: {Corr_avg}')
+                MAE_avg = test_results_list[0]['MAE'] 
+                result_avg1 += MAE_avg
+                print(f'key_eval: {key_eval}, missing rate: {cur_r}, MAE_avg: {MAE_avg}')
+            elif key_eval == 'Corr':
+                Corr_avg = test_results_list[0]['Corr'] 
+                result_avg1 += Corr_avg
+                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Corr_avg: {Corr_avg}')
             
         else:
-            for _, cur_seed  in enumerate([1111, 1112, 1113]):
-                best_ckpt = os.path.join(f'ckpt/{dataset_name}/best_{key_eval}_{cur_seed}.pth')
+            for _, cur_seed  in enumerate([1111]):
+                best_ckpt = os.path.join(f'/root/LNLN-main/ckpt/{dataset_name}/best_{key_eval}_{cur_seed}.pth')
                 model.load_state_dict(torch.load(best_ckpt)['state_dict'])
                 args['base']['missing_rate_eval_test'] = cur_r # Set missing rate
 
@@ -76,23 +88,39 @@ def main():
                 test_results_list.append(test_results_cur_seed)
 
             if key_eval == 'Has0_acc_2':
-                Has0_acc_2_avg = (test_results_list[0]['Has0_acc_2'] + test_results_list[1]['Has0_acc_2'] + test_results_list[2]['Has0_acc_2']) / 3
-                Has0_F1_score_avg = (test_results_list[0]['Has0_F1_score'] + test_results_list[1]['Has0_F1_score'] + test_results_list[2]['Has0_F1_score']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_2_avg: {Has0_acc_2_avg}, F1_score_avg: {Has0_F1_score_avg}')
+                Has0_acc_2_avg = (test_results_list[0]['Has0_acc_2'])
+                Has0_F1_score_avg = (test_results_list[0]['Has0_F1_score'])
+                print(
+                    f'key_eval: {key_eval}, missing rate: {cur_r}, Has0_acc_2_avg: {round(Has0_acc_2_avg, 4)}, F1_score_avg: {round(Has0_F1_score_avg, 4)}')
+                result_avg1 += Has0_acc_2_avg
+                result_arg2 += Has0_F1_score_avg
             elif key_eval == 'Non0_acc_2':
-                Non0_acc_2_avg = (test_results_list[0]['Non0_acc_2'] + test_results_list[1]['Non0_acc_2'] + test_results_list[2]['Non0_acc_2']) / 3
-                Non0_F1_score_avg = (test_results_list[0]['Non0_F1_score'] + test_results_list[1]['Non0_F1_score'] + test_results_list[2]['Non0_F1_score']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Non0_acc_2_avg: {Non0_acc_2_avg}, Non0_F1_score_avg: {Non0_F1_score_avg}')
+                Non0_acc_2_avg = (test_results_list[0]['Non0_acc_2'])
+                Non0_F1_score_avg = (test_results_list[0]['Non0_F1_score'])
+                print(
+                    f'key_eval: {key_eval}, missing rate: {cur_r}, Non0_acc_2_avg: {round(Non0_acc_2_avg, 4)}, Non0_F1_score_avg: {round(Non0_F1_score_avg, 4)}')
+                result_avg1 += Non0_acc_2_avg
+                result_arg2 += Non0_F1_score_avg
             elif key_eval == 'Mult_acc_5':
-                Mult_acc_5_avg = (test_results_list[0]['Mult_acc_5'] + test_results_list[1]['Mult_acc_5'] + test_results_list[2]['Mult_acc_5']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_5_avg: {Mult_acc_5_avg}')
+                Mult_acc_5_avg = (test_results_list[0]['Mult_acc_5'])
+                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_5_avg: {round(Mult_acc_5_avg, 4)}')
+                result_avg1 += Mult_acc_5_avg
             elif key_eval == 'Mult_acc_7':
-                Mult_acc_7_avg = (test_results_list[0]['Mult_acc_7'] + test_results_list[1]['Mult_acc_7'] + test_results_list[2]['Mult_acc_7']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_7_avg: {Mult_acc_7_avg}')
+                Mult_acc_7_avg = test_results_list[0]['Mult_acc_7'] 
+                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Mult_acc_7_avg: {round(Mult_acc_7_avg, 4)}')
+                result_avg1 += Mult_acc_7_avg
             elif key_eval == 'MAE':
-                MAE_avg = (test_results_list[0]['MAE'] + test_results_list[1]['MAE'] + test_results_list[2]['MAE']) / 3
-                Corr_avg = (test_results_list[0]['Corr'] + test_results_list[1]['Corr'] + test_results_list[2]['Corr']) / 3
-                print(f'key_eval: {key_eval}, missing rate: {cur_r}, MAE_avg: {MAE_avg}, Corr_avg: {Corr_avg}')
+                MAE_avg = test_results_list[0]['MAE'] 
+                #Corr_avg = test_results_list[0]['Corr'] 
+                print(
+                    f'key_eval: {key_eval}, missing rate: {cur_r}, MAE_avg: {round(MAE_avg, 4)}')
+                result_avg1 += MAE_avg
+                #result_arg2 += Corr_avg
+            elif key_eval == 'Corr':
+                Corr_avg = test_results_list[0]['Corr'] 
+                print(f'key_eval: {key_eval}, missing rate: {cur_r}, Corr_avg: {round(Corr_avg, 4)}')
+                result_avg1 += Corr_avg
+    print(f'result_avg1: {round(result_avg1 / 10, 4)}, result_arg2: {round(result_arg2 / 10, 4)}')
 
 
 def evaluate(model, eval_loader, metrics):
